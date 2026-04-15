5a2b30fe49ae50750853b9907caab9b5743c0040
avadeyn <manadineaborja@iskolarngbayan.pup.edu.ph>
2026-04-15 15:00:29 +0800

    Revise UI to remove filter functionality
    Update the Projects page to exclude the filter button.

--- src/pages/projects/SearchProjects.jsx
+++ src/pages/projects/SearchProjects.jsx
@@ -83,7 +83,7 @@ function SearchProjects({ projects = PROJECT_TITLES }) {
         </div>
 
         {/* Filter Button */}
-        <button
+        {/* <button
           type="button"
           className="flex items-center justify-center h-12 w-12 shrink-0 rounded-xl border border-white/40 bg-transparent text-white transition hover:bg-white/10"
           aria-label="Filter"
@@ -98,7 +98,7 @@ function SearchProjects({ projects = PROJECT_TITLES }) {
           >
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
           </svg>
-        </button>
+        </button> */}
       </div>
 
       {/* Results Area */}
